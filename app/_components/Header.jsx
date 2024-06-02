5a9669a504bc003ee4b4cf34694e4f8cf4c4441d
saketsanadhya <saketsanadhya2003@gmail.com>
2024-06-02 15:46:42 +0530

    authentication

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,13 +1,23 @@
+'use client'
 import { Button } from '@/components/ui/button'
+import { UserButton, useUser } from '@clerk/nextjs'
 import Image from 'next/image'
 import React from 'react'
 
 function Header() {
+  const {user,isSignedIn}=useUser()
   return (
     <div className='p-5 border-b shadow-sm'>
         <div className='flex items-center justify-between'>
-            <Image src={'/logo.svg'} width={150} height={50} alt='logo'/>   
+            <Image src={'/logo.svg'} width={150} height={50} alt='logo'/>
+            {isSignedIn?
+            <div className='flex items-center gap-5'>
+            <Button variant="outline">Dashboard</Button>
+            <UserButton/>
+            </div>:
             <Button>Get Started</Button>
+            }
+            
         </div>
     </div>
   )
